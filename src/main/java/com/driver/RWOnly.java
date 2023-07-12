134297ed90e17eb7f5e7c190b0240371b58c110a
Vikalpwebtech <vikalprautela@gmail.com>
2023-07-12 17:38:59 +0530

    Solved the Problem

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int name;
+
+    // Getter
+    public int getter() {
+        return name;
+    }
+
+    // Setter
+    public void setter (int newName) {
+        this.name = newName;
+    }
 }
